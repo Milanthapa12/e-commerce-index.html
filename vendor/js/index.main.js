be094198edb4d7c50f786203511a1da1b52085b4
unknown <milanthapait@gmail.com>
2020-07-22 17:44:14 +0545

    changes

---
+++ vendor/js/index.main.js
@@ -0,0 +1,62 @@
+
+const nextIcon = '<i class="fas fa-chevron-right"></i>';
+const prevIcon = '<i class="fas fa-chevron-left"></i>';
+
+$('.best_selling_items').owlCarousel({
+  loop:false,
+  // autoplay: true,
+  margin: 15,
+  dots: false,
+  nav: true,
+  navText: [
+  prevIcon,
+  nextIcon
+],
+  responsive:{
+    0:{
+          items:1
+      },
+      556:{
+        items: 2
+      },
+      768:{
+        items: 3
+      },
+      992:{
+        items: 4
+      },
+      1200:{
+          items:5
+      }
+  }
+});
+
+$('.brows-category').owlCarousel({
+  loop:true,
+  autoplay: true,
+  margin: 15,
+  dots: false,
+  nav: true,
+  navText: [
+  prevIcon,
+  nextIcon
+],
+  responsive:{
+    0:{
+          items:1
+      },
+      556:{
+        items: 2
+      },
+      768:{
+        items: 3
+      },
+      992:{
+        items: 4
+      },
+      1200:{
+          items:5
+      }
+  }
+});  
+  
